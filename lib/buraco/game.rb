a2f4e09183b2fe37cca798bcd824140cea8ebd74
Felipe Seixas <seixasfelipe@gmail.com>
2011-02-19 22:01:16 -0200

    did specs for start action to pass

--- lib/buraco/game.rb
+++ lib/buraco/game.rb
@@ -1,9 +1,12 @@
 module Buraco
   class Game
     def initialize(output)
+      @output = output
     end
 
     def start
+      @output.puts 'Bem vindo ao jogo de Buraco!'
+      @output.puts 'Jogador, digite o seu nome:'
     end
   end
 end
--- spec/buraco/game_spec.rb
+++ spec/buraco/game_spec.rb
@@ -3,16 +3,21 @@ require 'spec_helper'
 module Buraco
   describe Game do
     describe "#start" do
-      it "exibe mensagem de boa vinda" do
-        output = double('output')
-        game = Game.new(output)
+      
+      let(:output) { double('output').as_null_object }
+      let(:game) { Game.new(output) }
 
+      it "exibe mensagem de boa vinda" do
         output.should_receive(:puts).with('Bem vindo ao jogo de Buraco!')
 
         game.start
       end
 
-      it "solicita o nome do jogador"
+      it "solicita o nome do jogador" do
+        output.should_receive(:puts).with('Jogador, digite o seu nome:')
+
+        game.start
+      end
     end
   end
 end
