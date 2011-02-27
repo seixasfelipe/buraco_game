ca11580527bbaa4fa28da03518bc2b540361177a
Felipe Seixas <seixasfelipe@gmail.com>
2011-02-27 11:03:01 -0300

    implemented methods to manipulate cards on deck helper

--- lib/buraco/deck_helper.rb
+++ lib/buraco/deck_helper.rb
@@ -15,5 +15,12 @@ module Buraco
       cards.select {|c| c.suit == suit }      
     end
 
+    def self.sort(cards)
+      sorted_cards =      DeckHelper.select(cards, :of => :hearts)
+      sorted_cards.concat DeckHelper.select(cards, :of => :clubs)
+      sorted_cards.concat DeckHelper.select(cards, :of => :diamonds)
+      sorted_cards.concat DeckHelper.select(cards, :of => :spades)
+    end
+
   end
 end
