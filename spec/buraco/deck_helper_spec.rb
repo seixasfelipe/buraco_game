6d053a285c2ff4b340e5b4c9d3fb42a02a00fdff
Felipe Seixas <seixasfelipe@gmail.com>
2013-05-06 08:27:46 -0400

    some DRY and ruby 1.9 syntax

--- spec/buraco/deck_helper_spec.rb
+++ spec/buraco/deck_helper_spec.rb
@@ -4,27 +4,13 @@ module Buraco
   describe DeckHelper do
 
     def get_cards(options = {})
-      cards = [
-          Card.new(options[:of], 1), 
-          Card.new(options[:of], 2), 
-          Card.new(options[:of], 3), 
-          Card.new(options[:of], 4), 
-          Card.new(options[:of], 5), 
-          Card.new(options[:of], 6), 
-          Card.new(options[:of], 7), 
-          Card.new(options[:of], 8), 
-          Card.new(options[:of], 9), 
-          Card.new(options[:of], 10),
-          Card.new(options[:of], 11),
-          Card.new(options[:of], 12),
-          Card.new(options[:of], 13)
-      ]
+      cards = (1..13).map { |i| Card.new(options[:of], i)}
     end
 
     def cards_expectation_of(suit)
-        cards = DeckHelper.create_cards :of => suit
+        cards = DeckHelper.create_cards of: suit
         cards.should have_exactly(13).cards
-        cards.should be_eql get_cards :of => suit
+        cards.should be_eql get_cards of: suit
     end
 
     describe "#create_cards" do
@@ -48,10 +34,10 @@ module Buraco
     describe "#manipulate_cards" do
 
       it "deveria selecionar cartas de um dado naipe" do
-        cards =  get_cards(:of => :hearts)
-        cards.concat get_cards(:of => :clubs)
+        cards =  get_cards of: :hearts
+        cards.concat get_cards of: :clubs
 
-        selected_cards = DeckHelper.select(cards, :of => :clubs)
+        selected_cards = DeckHelper.select cards, of: :clubs
         
         selected_cards.should have_exactly(13).cards
         selected_cards.map do |c|
@@ -59,24 +45,24 @@ module Buraco
         end
       end
 
-      it "deveria selecionar nenhuma carta caso dao um naipe que nao exista na colecao" do
-        cards = get_cards(:of => :hearts)
+      it "deveria selecionar nenhuma carta dado um naipe que nao exista na colecao" do
+        cards = get_cards of: :hearts
 
-        selected_cards = DeckHelper.select(cards, :of => :clubs)
+        selected_cards = DeckHelper.select cards, of: :clubs
         selected_cards.should have_exactly(0).cards
         
-        selected_cards = DeckHelper.select(cards, :of => :diamonds)
+        selected_cards = DeckHelper.select cards, of: :diamonds
         selected_cards.should have_exactly(0).cards
       
-        selected_cards = DeckHelper.select(cards, :of => :spades)
+        selected_cards = DeckHelper.select cards, of: :spades
         selected_cards.should have_exactly(0).cards
       end
       
       it "deveria ordenar as cartas" do
-        cards =  get_cards(:of => :hearts)
-        cards.concat get_cards(:of => :clubs)
-        cards.concat get_cards(:of => :diamonds)
-        cards.concat get_cards(:of => :spades)
+        cards =  get_cards of: :hearts
+        cards.concat get_cards of: :clubs
+        cards.concat get_cards of: :diamonds
+        cards.concat get_cards of: :spades
 
         new_cards = cards.clone
         new_cards.should be_eql cards
