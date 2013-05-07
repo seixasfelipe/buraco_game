e70f0f147751600a8c9b43f38523afc17d6093e9
Felipe Seixas <seixasfelipe@gmail.com>
2013-05-06 20:46:17 -0400

    little refactoring

--- lib/buraco/state_machine.rb
+++ lib/buraco/state_machine.rb
@@ -2,20 +2,7 @@ module Buraco
   class StateMachine
 
     def initialize
-      @states = 
-      { 
-        new_deck:           :shuffle_deck,
-        shuffle_deck:       :give_dead_cards,
-        give_dead_cards:    :give_players_cards,
-        give_players_cards: :start_game,
-        start_game:         :change_player,
-        change_player:      :draw_card,
-        draw_card:          [:play_cards, :discard_card],
-        discard_card:       [:change_player, :end_game, :draw_dead_cards],
-        play_cards:         [:play_cards, :discard_card, :draw_dead_cards, :end_game],
-        draw_dead_cards:    [:play_cards, :discard_card],
-        end_game:           :calculate_points
-      }
+      @states = create_states
       @current_state = :new_deck
     end
 
@@ -32,6 +19,24 @@ module Buraco
 
     def skip_to(state)
       @current_state = state unless @states[state] == nil
+    end
+
+    private
+
+    def create_states
+      { 
+        new_deck:           :shuffle_deck,
+        shuffle_deck:       :give_dead_cards,
+        give_dead_cards:    :give_players_cards,
+        give_players_cards: :start_game,
+        start_game:         :change_player,
+        change_player:      :draw_card,
+        draw_card:          [:play_cards, :discard_card],
+        discard_card:       [:change_player, :end_game, :draw_dead_cards],
+        play_cards:         [:play_cards, :discard_card, :draw_dead_cards, :end_game],
+        draw_dead_cards:    [:play_cards, :discard_card],
+        end_game:           :calculate_points
+      }
     end
   end
 end
