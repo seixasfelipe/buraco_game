8414b87ee584a29239fb895d1d1b4a091156376c
Felipe Seixas <seixasfelipe@gmail.com>
2013-05-06 20:14:25 -0400

    adds more states

--- lib/buraco/state_machine.rb
+++ lib/buraco/state_machine.rb
@@ -2,7 +2,16 @@ module Buraco
   class StateMachine
 
     def initialize
-      @states = { new_deck: :shuffle_deck }
+      @states = 
+      { 
+        new_deck:           :shuffle_deck,
+        shuffle_deck:       :give_dead_cards,
+        give_dead_cards:    :give_players_cards,
+        give_players_cards: :start_game,
+        start_game:         :draw_card,
+        draw_card:          [:discard_card, :play_cards],
+        discard_card:       [:change_player, :end_game, :draw_dead_cards]
+      }
       @current_state = :new_deck
     end
 
@@ -10,9 +19,15 @@ module Buraco
       @current_state
     end
 
-    def next
-      @current_state = @states[@current_state]
+    def next(state = nil)
+      state ||= @current_state
+
+      @current_state = @states[state] unless @states[state] == nil
       @current_state
+    end
+
+    def skip_to(state)
+      @current_state = state unless @states[state] == nil
     end
   end
 end
--- spec/buraco/state_machine_spec.rb
+++ spec/buraco/state_machine_spec.rb
@@ -2,17 +2,64 @@ require 'spec_helper'
 
 module Buraco
   describe StateMachine do
-    describe "#states" do
+    
+    let(:state_machine) { StateMachine.new }
 
-      let(:state_machine) { StateMachine.new }
+    describe "#game states" do
 
       it "deveria ter criar novo deck como primeiro estado" do
         state_machine.current.should be_eql :new_deck
       end
 
-      it "deveria embaralhar as cartas do deck apos te-lo criado" do
+      it "deveria embaralhar as cartas do deck APOS te-lo criado" do
         state_machine.next
         state_machine.current.should be_eql :shuffle_deck
+      end
+
+      it "deveria distribuir os 2 mortos APOS embaralhar o deck" do
+        2.times { state_machine.next }
+        state_machine.current.should be_eql :give_dead_cards
+      end
+
+      it "deveria distribuir as cartas aos jogadores APOS distribuir o morto" do
+        3.times { state_machine.next }
+        state_machine.current.should be_eql :give_players_cards
+      end
+
+      it "deveria comecar o jogo APOS distribuir o morto" do
+        4.times { state_machine.next }
+        state_machine.current.should be_eql :start_game
+      end
+
+      it "deveria permanecer no mesmo estado caso nao exista o estado solicitado" do
+        state_machine.current.should be_eql :new_deck
+        state_machine.next :invalid_state
+        state_machine.current.should be_eql :new_deck
+      end
+    end
+
+
+    describe "#player states" do
+
+      it "deveria comprar uma carta pelo jogador da vez" do
+        state_machine.skip_to :start_game
+        state_machine.next
+
+        state_machine.current.should be_eql :draw_card
+      end
+
+      it "deveria descartar uma carta OU descer um jogo APOS comprar uma carta" do
+        state_machine.skip_to :draw_card
+        state_machine.next
+        
+        state_machine.current.should be_eql [:discard_card, :play_cards]
+      end
+
+      it "deveria mudar de jogador OU terminar o jogo OU comprar o morto APOS descartar uma carta" do
+        state_machine.skip_to :draw_card
+        state_machine.next :discard_card
+
+        state_machine.current.should be_eql [:change_player, :end_game, :draw_dead_cards]
       end
 
     end
